fc31cd9d9e14f7a77ea1015e89a646f35e1ca59d
Evgheniy Kovaliov <kovaliov.evgeniy11@gmail.com>
2023-12-19 09:16:32 +0200

    Change credentials

--- publish.sh
+++ publish.sh
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:kol-oss/<USERNAME>.github.io.git master
+# git push -f git@github.com:kovaliovev/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/kol-oss/database-coursework master:gh-pages
+git push -f https://github.com/kovaliovev/database-coursework master:gh-pages
 
 cd -
